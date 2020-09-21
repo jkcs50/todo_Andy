a9253b20774500757212625fbc2a91dce2071f40
JP <iwant2learn85@gmail.com>
2020-09-21 21:18:36 +0900

    added the ability to delete todo items and also added flash message

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -7,9 +7,19 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def destroy 
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:notice] = "The item #{@todo_item.content} on this todo list was deleted."
+    else
+      flash[:alert] = @todo_item.errors.full_messages[0]
+
+    end
+    redirect_to @todo_list
+  end
 
   private
-   
+    
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
   end
