4d877296b6279673a2db25735f69f383fc8ce8ae
JP <iwant2learn85@gmail.com>
2020-09-21 21:51:40 +0900

    added compeleted_at column to todo_items table

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,5 +1,6 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
+  before_action :set_todo_item, except: [:create]
 
   def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
@@ -8,7 +9,7 @@ class TodoItemsController < ApplicationController
   end
 
   def destroy 
-    @todo_item = @todo_list.todo_items.find(params[:id])
+    
     if @todo_item.destroy
       flash[:notice] = "The item #{@todo_item.content} on this todo list was deleted."
     else
@@ -18,10 +19,20 @@ class TodoItemsController < ApplicationController
     redirect_to @todo_list
   end
 
+  def complete
+    
+    @todo_item.update_attribute(:completed_at, Time.now)
+    redirect_to @todo_list, notice: "Todo item #{@todo_item.content} completed! Good job!"
+  end
+
   private
     
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
